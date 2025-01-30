6252de9a3d2e10ef6d184074c6dbf9ee37a2c643
sergiibuchkivskyi <fcmalin@gmail.com>
2025-01-30 16:54:37 +0200

    development: added test

---
+++ Google/GooglePage.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace SeleniumTests.Pages
+{
+    public class GooglePage
+    {
+        private readonly IWebDriver _driver;
+
+        private readonly By _searchBox = By.Name("q");
+        private readonly By _searchResults = By.Id("search");
+        private readonly By _firstResult = By.CssSelector("#search .g:first-child");
+
+        public GooglePage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void NavigateTo()
+        {
+            _driver.Navigate().GoToUrl("https://www.google.com");
+        }
+
+        public bool IsSearchBoxDisplayed()
+        {
+            return _driver.FindElement(_searchBox).Displayed;
+        }
+
+        public void EnterSearchText(string text)
+        {
+            var searchBox = _driver.FindElement(_searchBox);
+            searchBox.Clear();
+            searchBox.SendKeys(text);
+        }
+
+        public void PressEnter()
+        {
+            _driver.FindElement(_searchBox).SendKeys(Keys.Enter);
+        }
+
+        public bool AreSearchResultsDisplayed()
+        {
+            return _driver.FindElement(_searchResults).Displayed;
+        }
+
+        public string GetFirstResultText()
+        {
+            return _driver.FindElement(_firstResult).Text;
+        }
+    }
+}
---
+++ Google/GoogleSearchTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumTests.Pages;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class GoogleSearchTests
+    {
+        private IWebDriver _driver;
+        private GooglePage _googlePage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _googlePage = new GooglePage(_driver);
+        }
+
+        [Test]
+        public void SearchForSeleniumTutorial()
+        {
+            _googlePage.NavigateTo();
+
+            Assert.That(_googlePage.IsSearchBoxDisplayed(), "Search field is not available");
+
+            _googlePage.EnterSearchText("Selenium C# tutorial");
+
+            _googlePage.PressEnter();
+
+            Assert.That(_googlePage.AreSearchResultsDisplayed(), "Results are not displayed");
+
+            var firstResultText = _googlePage.GetFirstResultText();
+            Assert.That(firstResultText.Contains("Selenium"), "First result does not contain 'Selenium'");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+    }
+}
